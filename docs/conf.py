1f0163a6ff8a65f2436918c41ae38e1896aa80a7
Quentin DESMET <desmetque@gmail.com>
2025-03-28 17:05:55 +0100

    docs: initialize

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = "pyseasters"
+author = "Quentin Desmet"
+release = "1.0.0-alpha.1"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+]
+html_theme = "sphinx_rtd_theme"
---
+++ docs/index.rst
@@ -0,0 +1,11 @@
+Welcome to pySEASTERS Documentation
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   modules
+
+.. automodule:: pySEASTERS
+   :members:
