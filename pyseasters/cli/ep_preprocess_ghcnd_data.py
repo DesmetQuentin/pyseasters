58508837acbd7d661d404bd33ce243a52985962e
Quentin DESMET <desmetque@gmail.com>
2025-04-11 14:55:22 +0200

    feat(CLI): Add a --force option to avoid confirmation prompt

--- pyseasters/cli/ep_preprocess_ghcnd_data.py
+++ pyseasters/cli/ep_preprocess_ghcnd_data.py
@@ -6,7 +6,7 @@ Entry point for the ``preprocess_ghcnd_data`` command. The command has a help op
 .. code-block:: console
 
     $ preprocess_ghcnd_data --help
-    usage: preprocess_ghcnd_data [-h] [-m] [-v] [-s]
+    usage: preprocess_ghcnd_data [-h] [-m] [-v] [-s] [-f]
 
     Preprocess GHCNd data files (remove duplicate columns and compress).
 
@@ -15,6 +15,7 @@ Entry point for the ``preprocess_ghcnd_data`` command. The command has a help op
     -m, --minimal  keep orignial csv format (default: convert to parquet)
     -v, --verbose  enable debug output
     -s, --silent   disable info output (priority to --verbose)
+    -f, --force    disable confirmation prompt
 """
 
 import argparse
@@ -45,19 +46,26 @@ def main():
         action="store_true",
         help="disable info output (priority to --verbose)",
     )
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="disable confirmation prompt",
+    )
     args = parser.parse_args()
 
-    response = (
-        input(
-            "This program modifies files in place. Are you sure you want to continue? "
-            + "(y/[n]): "
+    if not args.force:
+        response = (
+            input(
+                "This program modifies files in place. Are you sure you want to continue? "
+                + "(y/[n]): "
+            )
+            .strip()
+            .lower()
         )
-        .strip()
-        .lower()
-    )
-    if response not in ("y", "yes"):
-        sys.stderr.write("Aborted by user.\n")
-        sys.exit(0)
+        if response not in ("y", "yes"):
+            sys.stderr.write("Aborted by user.\n")
+            sys.exit(0)
 
     setup_cli_logging(
         logging.DEBUG
--- pyseasters/cli/ep_preprocess_ghcnd_metadata.py
+++ pyseasters/cli/ep_preprocess_ghcnd_metadata.py
@@ -7,7 +7,7 @@ The command has a help option:
 .. code-block:: console
 
     $ preprocess_ghcnd_metadata --help
-    usage: preprocess_ghcnd_metadata [-h] [-m] [-v] [-s]
+    usage: preprocess_ghcnd_metadata [-h] [-m] [-v] [-s] [-f]
 
     Preprocess GHCNd metadata files (filter countries, remove duplicate columns and compress).
 
@@ -16,6 +16,7 @@ The command has a help option:
     -m, --minimal  keep orignial csv format (default: convert to parquet)
     -v, --verbose  enable debug output
     -s, --silent   disable info output (priority to --verbose)
+    -f, --force    disable confirmation prompt
 """
 
 import argparse
@@ -48,19 +49,26 @@ def main():
         action="store_true",
         help="disable info output (priority to --verbose)",
     )
+    parser.add_argument(
+        "-f",
+        "--force",
+        action="store_true",
+        help="disable confirmation prompt",
+    )
     args = parser.parse_args()
 
-    response = (
-        input(
-            "This program modifies files in place. Are you sure you want to continue? "
-            + "(y/[n]): "
+    if not args.force:
+        response = (
+            input(
+                "This program modifies files in place. Are you sure you want to continue? "
+                + "(y/[n]): "
+            )
+            .strip()
+            .lower()
         )
-        .strip()
-        .lower()
-    )
-    if response not in ("y", "yes"):
-        sys.stderr.write("Aborted by user.\n")
-        sys.exit(0)
+        if response not in ("y", "yes"):
+            sys.stderr.write("Aborted by user.\n")
+            sys.exit(0)
 
     setup_cli_logging(
         logging.DEBUG
